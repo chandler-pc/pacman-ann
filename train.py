4960e57514b1fea83d4e49231956ff1e144ae51e
Jhonnatan Espinoza Rojas <jhonnatan.espinoza.r@uni.pe>
2024-06-23 18:46:19 -0500

    fix: se modifico algoritmo genetico

--- train.py
+++ train.py
@@ -12,8 +12,9 @@ for generation in range(100):
         game = Game_NN(nn,5)
         game.run()
         fitness_scores.append(game.points)
-    print(f"Fitness Scores: {fitness_scores}")
-    ga.evolve(fitness_scores)
+        print("Points:", game.points)
+    print("Fitness Scores:", fitness_scores)
+    ga.evolve(fitness_scores, mutation_rate=0.2, tournament_size=3)
 
 best_nn = ga.population[np.argmax(fitness_scores)]
 pickle.dump(best_nn, open("best_nn.pkl", "wb"))
